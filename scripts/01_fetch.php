dfde58adfe339eb7777730aaa6b08de802be54b5
kiang <kiange@gmail.com>
2022-03-14 02:49:09 +0800

    new data

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -1,9 +1,9 @@
 <?php
-$rawPath = dirname(__DIR__) . '/raw';
+$rawPath = dirname(__DIR__) . '/raw/109';
 if(!file_exists($rawPath)) {
     mkdir($rawPath, 0777, true);
 }
-$dataset = json_decode(file_get_contents('https://data.tainan.gov.tw/api/3/action/package_search?q=par'), true);
+$dataset = json_decode(file_get_contents('https://data.tainan.gov.tw/api/3/action/package_search?q=109_par'), true);
 foreach($dataset['result']['results'][0]['resources'] AS $resource) {
     $rawFile = $rawPath . '/' . $resource['name'] . '.csv';
     file_put_contents($rawFile, file_get_contents($resource['url']));
